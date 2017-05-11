9945dd1bdabb0303e3895eedaee588bb03b2525e
mehdimehdi <mehdi@punchtab.com>
2017-05-10 21:49:00 -0700

    loading the templates from hardcoded html

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="dist")
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('boom.html')
 
 if __name__ == "__main__":
     app.run()

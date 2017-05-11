1153c21a73350d6e540616c36bff7208af159a18
mehdimehdi <mehdi@punchtab.com>
2017-05-11 14:45:42 -0700

    send from static

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder="frontend/build/")
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__, template_folder="frontend/build/",static_url_path='')
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('frontend/build/static/', path)
 
 if __name__ == "__main__":
     app.run()
